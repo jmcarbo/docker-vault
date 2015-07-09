26421a7672308e12b504968f15267ddc4467405b
Joan Marc Carbo Arnau <jmcarbo@gmail.com>
2015-07-09 06:40:22 +0200

    ...

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sleep 10
 export GOMAXPROCS=10
+consul agent -join consul &
 export MY_RANCHER_IP=$(ip addr | grep inet | grep 10.42 | tail -1 | awk '{print $2}' | awk -F\/ '{print $1}')
 if [ -z "${MY_RANCHER_IP}" ]; then
   echo "Can't find MY_RANCHER_IP ${MY_RANCHER_IP}"
